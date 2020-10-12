a84392d80910022da4670866bb66659dd3c8d1f3
John Sibly <John.Sibly@dazn.com>
2020-10-12 21:13:03 +0100

    Added checking for no duplicates per block

--- tests/sudoku.test.js
+++ tests/sudoku.test.js
@@ -3,6 +3,34 @@ const sudoku = require('../sudoku.js');
 beforeEach(() => {
 });
 
+function isPuzzleComplete(puzzle) {
+  const stringPuzzle = JSON.stringify(puzzle);
+  let isSolved = stringPuzzle.length == 181;
+
+  // check that each 3x3 cell contains 9 unique values
+  for(blockX = 0; isSolved && blockX < 9; blockX = blockX + 3 ) {
+    for(blockY = 0; isSolved && blockY < 9; blockY = blockY + 3) {
+        let cellValues = new Set();
+        cellValues.add(puzzle[blockY][blockX]);
+        cellValues.add(puzzle[blockY][blockX+1]);
+        cellValues.add(puzzle[blockY][blockX+2]);
+        cellValues.add(puzzle[blockY+1][blockX]);
+        cellValues.add(puzzle[blockY+1][blockX+1]);
+        cellValues.add(puzzle[blockY+1][blockX+2]);
+        cellValues.add(puzzle[blockY+2][blockX]);
+        cellValues.add(puzzle[blockY+2][blockX+1]);
+        cellValues.add(puzzle[blockY+2][blockX+2]);
+        console.log(cellValues);
+        isSolved = cellValues.size == 9;
+        if (!isSolved) {
+          console.log('stop');
+        }
+    }
+  }
+  
+  return isSolved;
+}
+
 it('Test puzzle 1 (easy)', async () => {
   const puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0], 
                   [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -14,9 +42,7 @@ it('Test puzzle 1 (easy)', async () => {
                   [0, 0, 0, 4, 1, 9, 0, 0, 5],
                   [0, 0, 0, 0, 8, 0, 0, 7, 9]];
   const solved = sudoku.solveSudoku(puzzle);
-  const stringPuzzle = JSON.stringify(solved);
-
-  expect(stringPuzzle.length).toEqual(181);
+  expect(isPuzzleComplete(solved)).toEqual(true);
 });
 
 it('Test puzzle 2 (easy)', async () => {
@@ -30,9 +56,7 @@ it('Test puzzle 2 (easy)', async () => {
                   [9, 0, 4, 0, 8, 0, 7, 0, 5],
                   [6, 0, 0, 1, 0, 7, 0, 0, 3]];
   const solved = sudoku.solveSudoku(puzzle);
-  const stringPuzzle = JSON.stringify(solved);
-
-  expect(stringPuzzle.length).toEqual(181);
+  expect(isPuzzleComplete(solved)).toEqual(true);
 });
 
 it('Test puzzle 3 (easy)', async () => {
@@ -46,8 +70,7 @@ it('Test puzzle 3 (easy)', async () => {
                   [8, 0, 0, 7, 0, 0, 0, 0, 6],
                   [0, 0, 0, 1, 9, 0, 7, 3, 0]];
   const solved = sudoku.solveSudoku(puzzle);
-  const stringPuzzle = JSON.stringify(solved);
-  expect(stringPuzzle.length).toEqual(181);
+  expect(isPuzzleComplete(solved)).toEqual(true);
 });
 
 it('Test puzzle 4 (medium)', async () => {
@@ -61,8 +84,7 @@ it('Test puzzle 4 (medium)', async () => {
                   [0, 0, 0, 7, 0, 9, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0, 2, 0, 6]];
   const solved = sudoku.solveSudoku(puzzle);
-  const stringPuzzle = JSON.stringify(solved);
-  expect(stringPuzzle.length).toEqual(181);
+  expect(isPuzzleComplete(solved)).toEqual(true);
 });
 
 it('Test puzzle 5 (medium)', async () => {
@@ -76,8 +98,7 @@ it('Test puzzle 5 (medium)', async () => {
                   [0, 9, 4, 0, 0, 1, 5, 0, 0],
                   [0, 0, 7, 8, 4, 0, 0, 0, 2]];
   const solved = sudoku.solveSudoku(puzzle);
-  const stringPuzzle = JSON.stringify(solved);
-  expect(stringPuzzle.length).toEqual(181);
+  expect(isPuzzleComplete(solved)).toEqual(true);
 });
 
 it('Test puzzle 6 (hard)', async () => {
@@ -91,8 +112,7 @@ it('Test puzzle 6 (hard)', async () => {
                   [0, 8, 0, 0, 0, 6, 0, 0, 0],
                   [0, 0, 2, 0, 7, 0, 0, 0, 5]];
   const solved = sudoku.solveSudoku(puzzle);
-  const stringPuzzle = JSON.stringify(solved);
-  expect(stringPuzzle.length).toEqual(181);
+  expect(isPuzzleComplete(solved)).toEqual(true);
 });
 
 it('Test puzzle 7 (evil)', async () => {
@@ -106,8 +126,7 @@ it('Test puzzle 7 (evil)', async () => {
                   [2, 0, 0, 0, 0, 0, 6, 0, 0],
                   [0, 0, 0, 0, 2, 0, 0, 5, 0]];
   const solved = sudoku.solveSudoku(puzzle);
-  const stringPuzzle = JSON.stringify(solved);
-  expect(stringPuzzle.length).toEqual(181);
+  expect(isPuzzleComplete(solved)).toEqual(true);
 });
 
 it('Test puzzle 8 (hard from Take a break magazine)', async () => {
@@ -121,8 +140,7 @@ it('Test puzzle 8 (hard from Take a break magazine)', async () => {
                   [3, 0, 0, 8, 0, 2, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0, 0, 5, 3]];
   const solved = sudoku.solveSudoku(puzzle);
-  const stringPuzzle = JSON.stringify(solved);
-  expect(stringPuzzle.length).toEqual(181);
+  expect(isPuzzleComplete(solved)).toEqual(true);
 });
 
 it('Test puzzle 9 (hard from Take a break magazine)', async () => {
@@ -136,8 +154,7 @@ it('Test puzzle 9 (hard from Take a break magazine)', async () => {
                   [0, 0, 6, 4, 0, 3, 0, 7, 0],
                   [0, 5, 0, 0, 0, 0, 0, 0, 0]];
   const solved = sudoku.solveSudoku(puzzle);
-  const stringPuzzle = JSON.stringify(solved);
-  expect(stringPuzzle.length).toEqual(181);
+  expect(isPuzzleComplete(solved)).toEqual(true);
 });
 
 it('Test puzzle 10 (hard from Take a break magazine)', async () => {
@@ -151,6 +168,5 @@ it('Test puzzle 10 (hard from Take a break magazine)', async () => {
                   [0, 3, 0, 0, 0, 0, 0, 6, 0],
                   [1, 4, 0, 0, 0, 2, 0, 0, 0]];
   const solved = sudoku.solveSudoku(puzzle);
-  const stringPuzzle = JSON.stringify(solved);
-  expect(stringPuzzle.length).toEqual(181);
+  expect(isPuzzleComplete(solved)).toEqual(true);
 });
